5876d765984200af9c9c18c0d3392fc628a6a18d
JDMTias <matiasiturbide@gmail.com>
2020-08-25 13:02:23 -0600

    packages added

---
+++ src/main/java/com/matiasiturbide/medicinecabinet/MedicinecabinetApplication.java
@@ -0,0 +1,13 @@
+package com.matiasiturbide.medicinecabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicinecabinetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicinecabinetApplication.class, args);
+    }
+
+}
